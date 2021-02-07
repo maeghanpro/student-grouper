e027892b12d27da30fee0bb25bb08fe919a91ada
maeghanpro <maeghan.m.provencher@gmail.com>
2021-02-07 11:01:44 -0500

    class index feature

---
+++ client/src/components/ClassSectionTile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, CardContent, CardActions, Button, Typography } from '@material-ui/core'
+
+const ClassSectionTile = ({classSection}) => {
+  const colorOptions = ["#795061", "#212E49", "#39565A", "#315E78", "#7C6764", "#7B717C", "#2E3F5A", "#908C5A"]
+  const randomIndex = Math.floor(Math.random() * colorOptions.length)
+  const randomColor = colorOptions[randomIndex]
+  const style = {
+    background: randomColor,
+    color: 'white'
+  }
+  return (
+    <Card className="class-card" style={style} raised>
+      <CardContent>
+        <Typography variant="h3">{classSection.name}</Typography>
+      </CardContent>
+      <CardActions className="class-card-buttons" >
+        <Button variant="contained" size="large">Students</Button>
+        <Button variant="contained" size="large">Groups</Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default ClassSectionTile
--- server/src/routes/api/v1/classSectionsRouter.js
+++ server/src/routes/api/v1/classSectionsRouter.js
@@ -1,9 +1,18 @@
 import express from 'express'
 
+import { User } from '../../../models/index.js'
+
 const classSectionsRouter = new express.Router()
 
-classSectionsRouter.get('/', (req, res) => {
-  res.send('Class Data')
+classSectionsRouter.get('/', async (req, res) => {
+  const userId = req.user.id
+  try {
+    const user = await User.query().findById(userId)
+    const classSections = await user.$relatedQuery('classSections')
+    return res.status(200).json({classSections})
+  } catch (error) {
+    return res.status(500).json({errors: error})
+  }
 })
 
 export default classSectionsRouter
