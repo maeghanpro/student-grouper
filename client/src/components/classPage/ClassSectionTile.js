c98f395a07a58b43acdf60faafd9ddb45d4f4ace
maeghanpro <maeghan.m.provencher@gmail.com>
2021-02-09 16:27:59 -0500

    edit class buttons

--- client/src/components/classPage/ClassSectionTile.js
+++ client/src/components/classPage/ClassSectionTile.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
-import { Card, CardContent, CardActions, Button, Typography } from '@material-ui/core'
+import { Card, CardContent, CardActions, Button, Typography, IconButton } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit';
 
 const ClassSectionTile = ({classSection}) => {
   const colorOptions = ["#795061", "#212E49", "#39565A", "#315E78", "#7C6764", "#7B717C", "#2E3F5A", "#908C5A"]
@@ -10,9 +11,17 @@ const ClassSectionTile = ({classSection}) => {
     background: randomColor,
     color: '#F3F3EE'
   }
+  const [editable, setEditable] = useState(false)
+
+  const handleEdit = () => {
+    setEditable(true)
+  } 
   
   return (
     <Card className="class-card" style={style} raised>
+      <IconButton className="edit-class-icon" aria-label="edit" color="inherit" onClick={handleEdit}>
+        <EditIcon />
+      </IconButton>
       <CardContent>
         <Typography variant="h3">{classSection.name}</Typography>
       </CardContent>
