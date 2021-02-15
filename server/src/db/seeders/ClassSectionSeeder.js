b5be60a99255ff9ee9d9bb621c00023654a06250
maeghanpro <maeghan.m.provencher@gmail.com>
2021-02-15 17:54:12 -0500

    classSection tile colors persist

--- server/src/db/seeders/ClassSectionSeeder.js
+++ server/src/db/seeders/ClassSectionSeeder.js
@@ -8,30 +8,37 @@ class ClassSectionSeeder {
     const classSectionData = [
       {
         name: '7A',
+        color: '#93995F',
         userId: userOne.id
       },
       {
         name: '7B',
+        color: '#315E78',
         userId: userOne.id
       },
       {
         name: '7C',
+        color: '#2E3F5A',
         userId: userOne.id
       },
       {
         name: '7D',
+        color: '#7B717C',
         userId: userOne.id
       },
       {
         name: '6A',
+        color: '#795061',
         userId: userTwo.id
       },
       {
         name: '6B',
+        color: '#E48B6B',
         userId: userTwo.id
       },
       {
         name: '6C',
+        color: '#2E3F5A',
         userId: userTwo.id
       }
     ]
