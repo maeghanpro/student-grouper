0f875c56b4016d86d2b782bdbbe6f44ef7995694
maeghanpro <maeghan.m.provencher@gmail.com>
2021-02-20 18:15:57 -0500

    tests for getGroupSizeOptions

--- server/src/boot/environments/development.js
+++ server/src/boot/environments/development.js
@@ -1,11 +1,10 @@
 import getNodeEnv from "../../config/getNodeEnv.js";
 
-// (async () => {
   if (getNodeEnv() === "development") {
     // development specific middlewares here
     const { default: dotenv } = await import("dotenv");
     await dotenv.config();
   }
-// })()
+
 
 
