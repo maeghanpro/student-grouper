68673b071d2958cdc06f34c5d8a2b102967ef421
maeghanpro <maeghan.m.provencher@gmail.com>
2021-02-20 15:52:53 -0500

    tests for getEvenGroups method

--- server/__tests__/services/Grouping.spec.js
+++ server/__tests__/services/Grouping.spec.js
@@ -66,6 +66,7 @@ describe('src/services/Grouping.js', () => {
         { id: 14, rating: 3 }
       ]
     })
+
     it('creates the correct number of groups of the correct size', () => {
       const sizeOptions = getGroupSizeOptions(students)
       
@@ -77,7 +78,7 @@ describe('src/services/Grouping.js', () => {
         expect(actual).toHaveLength(arrayLength)
         // asserts that each group is the correct size
         // when user requests pairs, the expected group size will be 2 or 3
-        // otherwise the expected group size will be equal to or one greater
+        // otherwise the expected group size will be equal to the specified size or one less
         actual.forEach(group => {
           if (size === 2) {
             expect(group.length === size || group.length === size + 1).toBe(true)
@@ -98,4 +99,83 @@ describe('src/services/Grouping.js', () => {
       ])
     })
   });
+
+  describe('getEvenGroups', () => {
+    let students;
+    let size;
+    beforeEach(() => {
+      students = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+      size = 3
+    })
+
+    it ('calls the lodash chunk method', () => {
+      Grouping.getEvenGroups(size, students)
+
+      expect(chunk.mock.calls).toHaveLength(1)
+      expect(chunk.mock.calls[0][0]).toEqual(students)
+      expect(chunk.mock.calls[0][1]).toBe(size)
+    })
+    
+    it ('case 1: number of students is evenly divisible by group size', () => {
+      expect(Grouping.getEvenGroups(size, students)).toEqual([
+        [1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]
+      ])
+    })
+
+    it ('case 2: number of students divided by group size has remainder of size - 1', () => {
+      size = 4
+
+      expect(Grouping.getEvenGroups(size, students)).toEqual([
+        [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15]
+      ])
+    })
+
+    it ('case 3: number of students divided by group size has remainder less than size - 1, but greater than 0', () => {
+      size = 6
+      
+      expect(Grouping.getEvenGroups(size, students)).toEqual([
+        [1, 2, 3, 4, 5], [7, 8, 9, 10, 11], [13, 14, 15, 6, 12]
+      ])
+    })
+
+    it ('case 4: size - remainder is greater than the number of groups', () => {
+      // returns a multidimensional array, but groups will not be even
+      // in practice this situation cannot happen due to the getGroupSizeOptions function 
+      size = 7
+
+      expect(Grouping.getEvenGroups(size, students)).toEqual([
+        [1, 2, 3, 4, 5, 6], [8, 9, 10, 11, 12, 13], [15, 7, 14]
+      ])
+    })
+
+    it('returns groups of 2 and one group of 3 when user requests pairs with an odd number of students', () => {
+      const size = 2
+      const actual = Grouping.getEvenGroups(size, students)
+
+      actual.forEach( (group, index) => {
+        if (index === actual.length - 1) {
+          expect(group).toHaveLength(size + 1)
+        } else {
+          expect(group).toHaveLength(size)
+        }
+      })
+    })
+
+    it('returns groups where the number of students in each group equals the size or one less', () => {
+      const sizeOptions = getGroupSizeOptions(students)
+
+      sizeOptions.map(size => {
+        const actual = Grouping.getEvenGroups(size, students)
+
+        // asserts that each group is the correct size
+        // when user requests pairs, the expected group size will be 2 or 3 tested above
+        // otherwise the expected group size will be equal to the specified size or one less
+        actual.forEach(group => {
+          if (size > 2) {
+            expect(group.length === size || group.length === size - 1).toBe(true)
+          }
+        })
+      })
+    })
+  })
 });
